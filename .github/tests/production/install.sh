4105d310a70c426afa144e40b81373a485224c95
Kevin Fox <Kevin.Fox@pnnl.gov>
2023-03-17 15:04:49 -0700

    Fork the lockdown test to reflect its also testing namespace overrides.
    Also rename the lockdown test production to match that its testing the
    examples/production settings.
    
    Signed-off-by: Kevin Fox <Kevin.Fox@pnnl.gov>

---
+++ .github/tests/production/install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+kubectl create namespace spire-system
+kubectl label namespace spire-system pod-security.kubernetes.io/enforce=privileged
+kubectl create namespace spire-server
+kubectl label namespace spire-server pod-security.kubernetes.io/enforce=restricted
+
+helm upgrade --install --namespace spire-server spire charts/spire -f examples/production/values.yaml
