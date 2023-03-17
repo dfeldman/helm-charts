4a714895e407691d920c6ad7f1af6cdb6708828d
Kevin Fox <Kevin.Fox@pnnl.gov>
2023-03-17 16:27:32 -0700

    ingress-nginx production example
    This patch adds a production addon values file with best practices
    for using ingress-nginx.
    
    Signed-off-by: Kevin Fox <Kevin.Fox@pnnl.gov>

---
+++ .github/tests/production-ingress-nginx/install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+kubectl create namespace spire-system
+kubectl label namespace spire-system pod-security.kubernetes.io/enforce=privileged
+kubectl create namespace spire-server
+kubectl label namespace spire-server pod-security.kubernetes.io/enforce=restricted
+
+helm install cert-manager cert-manager --namespace cert-manager --create-namespace --version v1.11.0 --set installCRDs=true --repo https://charts.jetstack.io --wait
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+kubectl apply -f $SCRIPT_DIR/testcert.yaml -n spire-server
+
+helm install ingress-nginx ingress-nginx --version 4.5.2 --repo https://kubernetes.github.io/ingress-nginx --create-namespace -n ingress-nginx --set controller.extraArgs.enable-ssl-passthrough=
+
+helm upgrade --install --namespace spire-server spire charts/spire -f examples/production/values.yaml -f examples/production/values-export-ingress-nginx.yaml --wait
+
+ip=$(kubectl get svc -n ingress-nginx ingress-nginx-controller -o go-template='{{ .spec.clusterIP }}')
+echo ip oidc-discovery.example.org
+
+kubectl get secret -n spire-server tls-cert -o go-template='{{ index .data "ca.crt" | base64decode }}' > /tmp/ca
+
+curl --cacert /tmp/ca --resolve oidc-discovery.example.org:443:$ip https://oidc-discovery.example.org/.well-known/openid-configuration
--- examples/production/README.md
+++ examples/production/README.md
@@ -5,4 +5,12 @@ kubectl label namespace "spire-system" pod-security.kubernetes.io/enforce=privil
 kubectl create namespace "spire-server"
 kubectl label namespace "spire-server" pod-security.kubernetes.io/enforce=restricted
 
+```
 helm upgrade --install --namespace spire-server spire charts/spire -f values.yaml
+```
+
+If your using ingress-nginx and want to expose spire outside the cluster, add the
+following to the end of the helm upgrade example:
+```
+-f values-export-ingress-nginx.yaml
+```
