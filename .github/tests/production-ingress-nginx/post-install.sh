66a809c77db75e8bc3a8f6ffa6e429888963381d
Kevin Fox <Kevin.Fox@pnnl.gov>
2023-03-18 00:48:37 -0700

    More logging. Turn off ingress webhook.
    Signed-off-by: Kevin Fox <Kevin.Fox@pnnl.gov>

--- .github/tests/production-ingress-nginx/post-install.sh
+++ .github/tests/production-ingress-nginx/post-install.sh
@@ -19,30 +19,21 @@ cat <<EOF >>"$GITHUB_STEP_SUMMARY"
 EOF
 
 if [ $1 -ne 0 ]; then
-  echo
-  echo '```'
-  echo '==> Events of namespace spire-server'
-  echo '........................................................................................................................'
-  echo '>>> kubectl --request-timeout=30s get events --output wide --namespace spire-server'
-  kubectl --request-timeout=30s get events --output wide --namespace spire-server
-  echo '........................................................................................................................'
-  echo '<== Events of namespace spire-server'
-  echo '........................................................................................................................'
-  echo '>>> kubectl --request-timeout=30s describe pods --namespace spire-server'
-  kubectl --request-timeout=30s describe pods --namespace spire-server
-  echo '========================================================================================================================'
-  echo '==> Events of namespace spire-system'
-  echo '........................................................................................................................'
-  echo '>>> kubectl --request-timeout=30s get events --output wide --namespace spire-system'
-  kubectl --request-timeout=30s get events --output wide --namespace spire-system
-  echo '........................................................................................................................'
-  echo '<== Events of namespace spire-system'
-  echo '........................................................................................................................'
-  echo '>>> kubectl --request-timeout=30s describe pods --namespace spire-system'
-  kubectl --request-timeout=30s describe pods --namespace spire-system
-  echo '========================================================================================================================'
-  kubectl get pods -o name -n spire-server | while read line; do echo logs for $line; kubectl logs -n spire-server $line --all-containers=true --ignore-errors=true; done
-  kubectl get pods -o name -n spire-system | while read line; do echo logs for $line; kubectl logs -n spire-system $line --all-containers=true --ignore-errors=true; done
-  echo '========================================================================================================================'
-  echo '```'
+  for ns in spire-server spire-system ingress-nginx cert-manager; do
+    echo
+    echo '```'
+    echo "==> Events of namespace $ns"
+    echo "........................................................................................................................"
+    echo ">>> kubectl --request-timeout=30s get events --output wide --namespace $ns"
+    kubectl --request-timeout=30s get events --output wide --namespace $ns
+    echo "........................................................................................................................"
+    echo "<== Events of namespace $ns"
+    echo "........................................................................................................................"
+    echo ">>> kubectl --request-timeout=30s describe pods --namespace $ns"
+    kubectl --request-timeout=30s describe pods --namespace $ns
+    echo "========================================================================================================================"
+    kubectl get pods -o name -n $ns | while read line; do echo logs for $line; kubectl logs -n $ns $line --all-containers=true --ignore-errors=true; done
+    echo '========================================================================================================================'
+    echo '```'
+  done
 fi | cat >> "$GITHUB_STEP_SUMMARY"
